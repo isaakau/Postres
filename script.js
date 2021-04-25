fd61e15810d50eca37485433de51d894596ce3e5
Isabella <isabella.moreno.bc@gmail.com>
2021-04-25 17:58:55 -0400

    se agregan validaciones de jquery

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
+(function() {
     'use strict'
 
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
@@ -7,14 +7,58 @@
 
     // Loop over them and prevent submission
     Array.prototype.slice.call(forms)
-        .forEach(function (form) {
-            form.addEventListener('submit', function (event) {
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
                 if (!form.checkValidity()) {
-                event.preventDefault()
-                event.stopPropagation()
-            }
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
 
-            form.classList.add('was-validated')
+                form.classList.add('was-validated')
             }, false)
         })
-})() 
+})
+
+$(document).ready(function() {
+    $("#formularioContacto").validate({
+        rules: {
+            nombre: {
+                required: true,
+                minlenght: 3
+            },
+            apellido: {
+                required: true,
+                minlenght: 3
+            },
+            correo: {
+                required: true,
+                email: true
+            },
+            telefono: {
+                required: true,
+                number: true
+            }
+
+        },
+        messages: {
+            nombre: {
+                required: "Debe ingresar su nombre",
+                minlenght: "El nombre debe tener mínimo 3 caracteres"
+            },
+            apellido: {
+                required: "Debe ingresar su apellido",
+                minlenght: "El apellido debe tener mínimo 3 caracteres"
+            },
+            correo: {
+                required: "Debe ingresar su correo electrónico",
+                email: "El email debe tener un formato válido"
+            },
+            telefono: {
+                required: "Debe ingresar su teléfono",
+                number: "Debe ingresar sólo números"
+            }
+        }
+
+    });
+
+});
